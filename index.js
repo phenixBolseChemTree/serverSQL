4b051f83347c9cf0dcedd492af870298b820c512
Ivan Sharapov <oldrum75@gmail.com>
2024-02-22 13:32:25 +0100

    fix: use nodemon

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const PORT = process.env.PORT || 8080
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.send('NODEMON WORK')
+})
+
 app.listen(
   PORT,
   () => console.log(`server started on port ${PORT}`))
 
-
-// console.log(PORT);
